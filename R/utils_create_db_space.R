4201f97b34d7f22094f3f650847d59952021299b
Your Name <jeffrey.sj.tang@gmail.com>
2025-12-04 16:26:18 -0800

    fix create_db_space by making sure the objects get captured into a list for save to work

--- R/utils_create_db_space.R
+++ R/utils_create_db_space.R
@@ -8,23 +8,24 @@
 #' @return Saves the database list
 #' @export
 #'
-#' @examples
 
-create_db_space <- function(..., 
-                            filePath = "data/ligand_receptor_db.rda", 
-                            saveToFile = TRUE) {  
+create_db_space <- function(...,
+                            filePath = "data/ligand_receptor_db.rda",
+                            saveToFile = TRUE) {
   # Check if the data directory exists might need to come back to this
   data_dir <- dirname(filePath)
   if (!dir.exists(data_dir)) {
-    stop(paste("The directory", 
-               data_dir, 
+    stop(paste("The directory",
+               data_dir,
                "does not exist. Please create it first."))
   }
-  
+
   # Save the database list to the file if the save_to_file parameter is TRUE
   if (saveToFile) {
+    # Capture the objects passed via ...
+    objs <- list(...)
     # Save the list to the specified file path
-    save(..., file = filePath)
+    save(list = names(objs), file = filePath, envir = list2env(objs))
     message("Database list saved to ", filePath)
   } else {
     message("Database list not saved.")
